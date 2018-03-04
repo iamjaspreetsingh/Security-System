4e297dd315038144d07e9b765b5a5f5698844a14
iamjaspreetsingh <jsk1961998@gmail.com>
2018-03-04 21:48:56 +0530

    added few other buttons

--- app/src/main/java/bvpieee/securitysystem/MainActivity.java
+++ app/src/main/java/bvpieee/securitysystem/MainActivity.java
@@ -1,11 +1,16 @@
 package bvpieee.securitysystem;
 
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.content.pm.ActivityInfo;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.v7.app.AlertDialog;
+import android.support.v7.app.AppCompatActivity;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.Switch;
 
@@ -18,7 +23,23 @@ public class MainActivity extends AppCompatActivity {
 
         setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
 
+final FloatingActionButton fb=(FloatingActionButton)findViewById(R.id.floatingActionButton);
+fb.setOnClickListener(new View.OnClickListener() {
+    @Override
+    public void onClick(View view) {
 
+
+
+
+
+        adddialog();
+
+
+
+
+
+    }
+});
         final Switch activate=(Switch)findViewById(R.id.switch1);
         final Switch alarm=(Switch)findViewById(R.id.switch2);
 
@@ -133,6 +154,61 @@ public class MainActivity extends AppCompatActivity {
 
 
 
+
+    }
+
+    private void adddialog() {
+
+
+
+        LayoutInflater inflater = getLayoutInflater();
+        View alertLayout = inflater.inflate(R.layout.layout, null);
+        final EditText num=(EditText)alertLayout.findViewById(R.id.no);
+
+
+
+        SharedPreferences preference=getSharedPreferences("ieee",MODE_PRIVATE);
+        String acce=preference.getString("no","");
+
+        num.setText(acce);
+
+        SharedPreferences.Editor editor=getSharedPreferences("ieee",MODE_PRIVATE).edit();
+        editor.putString("no",num.toString());
+
+        editor.apply();
+
+        AlertDialog.Builder alert = new AlertDialog.Builder(this);
+
+        // this is set the view from XML inside AlertDialog
+        alert.setView(alertLayout);
+        // disallow cancel of AlertDialog on click of back button and outside touch
+        alert.setTitle("Contact ");
+        alert.setIcon(R.drawable.ic_perm_contact_calendar_black_24dp);
+        alert.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+
+            }
+        });
+
+
+        alert.setPositiveButton("Set", new DialogInterface.OnClickListener() {
+
+
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+
+
+            }
+        });
+        AlertDialog dialog = alert.create();
+        dialog.show();
+
+
+
+
+        
 
     }
 
